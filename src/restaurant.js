68721f4afd525000d9394e949d9c97df3d72e70a
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-08-18 14:43:51 -0300

    Iniciando resolução do requisito 10

--- src/restaurant.js
+++ src/restaurant.js
@@ -93,6 +93,9 @@
 // - retornará o valor somado acrescido de 10%.
 // DICA: para isso, você precisará percorrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-const createMenu = () => {};
+const createMenu = (request) => ({
+    fetchMenu: () => request,
+    consumption: [],
+  });
 
 module.exports = createMenu;
